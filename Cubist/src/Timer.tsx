56e1bf3d56a053e1458597779695c7f9d598c079
Vic <victoriawoo2005@gmail.com>
2024-11-16 02:01:11 -0500

    fixed missing semicolons

--- Cubist/src/Timer.tsx
+++ Cubist/src/Timer.tsx
@@ -1,67 +1,67 @@
 import * as React from "react";
 
 type TimerProps = {
-    time: number;
+  time: number;
 };
 
 function Timer({ time }: TimerProps) {
-    const [min, setMin] = React.useState<number>(0);
-    const [sec, setSec] = React.useState<number>(0);
-    const [msec, setMsec] = React.useState<number>(0);
-    const [isRunning, setIsRunning] = React.useState<boolean>(false);
-    const [currentTime, setCurrentTime] = React.useState<number>(0);
-    
-    const makeTimeForm = (time: number): void => {
-        const minutes = Math.floor(time / 60);
-        const seconds = Math.floor(time % 60)
-        const milliseconds = Math.floor((time % 1) * 100);
+  const [min, setMin] = React.useState<number>(0);
+  const [sec, setSec] = React.useState<number>(0);
+  const [msec, setMsec] = React.useState<number>(0);
+  const [isRunning, setIsRunning] = React.useState<boolean>(false);
+  const [currentTime, setCurrentTime] = React.useState<number>(0);
 
-        setMin(minutes);
-        setSec(seconds);
-        setMsec(milliseconds);
-    };
+  const makeTimeForm = (time: number): void => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    const milliseconds = Math.floor((time % 1) * 100);
 
-    const handleKeyPress = (e: KeyboardEvent) => {
-        if (e.code === 'Space') {
-            if (!isRunning) {
-                setCurrentTime(0);
-            }
-            setIsRunning((prev) => !prev);
-        }
+    setMin(minutes);
+    setSec(seconds);
+    setMsec(milliseconds);
+  };
+
+  const handleKeyPress = (e: KeyboardEvent) => {
+    if (e.code === 'Space') {
+      if (!isRunning) {
+        setCurrentTime(0);
+      }
+      setIsRunning((prev) => !prev);
     }
+  };
 
-    React.useEffect(() => {
-        if (isRunning) {
-            const interval = setInterval(() => {
-                setCurrentTime((prevTime) => {
-                    const newTime = prevTime + 0.01;
-                    makeTimeForm(newTime);
-                    return newTime;
-                });
-            }, 10);
+  React.useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setCurrentTime((prevTime) => {
+          const newTime = prevTime + 0.01;
+          makeTimeForm(newTime);
+          return newTime;
+        });
+      }, 10);
 
-            return () => clearInterval(interval);
-        }
-    }, [isRunning]);
+      return () => clearInterval(interval);
+    }
+  }, [isRunning]);
 
-    React.useEffect(() => {
-        window.addEventListener("keydown", handleKeyPress);
-        return () => {
-            window.removeEventListener("keydown", handleKeyPress);
-        };
-    }, []);
+  React.useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
 
-    const padZero = (num: number): string => num.toString().padStart(2, '0');
+  const padZero = (num: number): string => num.toString().padStart(2, '0');
 
-    return (
-        <div>
-            <span className="time">{padZero(min)}</span>
-            <span className="colon">:</span>
-            <span className="time">{padZero(sec)}</span>
-            <span className="period">.</span>
-            <span className="time">{padZero(msec)}</span>
-        </div>
-    );
+  return (
+    <div>
+      <span className="time">{padZero(min)}</span>
+      <span className="colon">:</span>
+      <span className="time">{padZero(sec)}</span>
+      <span className="period">.</span>
+      <span className="time">{padZero(msec)}</span>
+    </div>
+  );
 }
 
 export default Timer;
