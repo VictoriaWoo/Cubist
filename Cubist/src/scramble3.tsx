b8f50ca60dc0c9ff46fbab31792c6a0b3fa680dc
Vic <victoriawoo2005@gmail.com>
2024-11-30 15:39:27 -0500

    generate + display scramble, favicon, site title

--- Cubist/src/scramble3.tsx
+++ Cubist/src/scramble3.tsx
@@ -1,13 +1,59 @@
-// import * as React from "react";
+import React, { useState, useEffect } from 'react';
 
-const edges = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-const corners = [0, 1, 2 ,3, 4, 5, 6, 7];
-
-const moves = [ 'U', 'U\'', 'U2', 'D', 'D\'', 'D2', 'L', 'L\'', 'L2', 'R', 'R\'', 'R2', 'F', 'F\'', 'F2', 'B', 'B\'', 'B2',];
+const moves = ['U', 'U\'', 'U2', 'D', 'D\'', 'D2', 'L', 'L\'', 'L2', 'R', 'R\'', 'R2', 'F', 'F\'', 'F2', 'B', 'B\'', 'B2',];
 
 function getRandomMove(): string {
-    const randomIndex = Math.floor(Math.random() * moves.length);
-    return moves[randomIndex];
+  return moves[Math.floor(Math.random() * moves.length)];
 }
 
-console.log(getRandomMove());
+function getFace(move: string): string {
+  return move.charAt(0);
+}
+
+function generateScramble(length: number): string {
+  let scramble = '';
+  let lastMove: string | null = null;
+
+  for (let i = 0; i < length; i++) {
+    let move: string;
+
+    do {
+      move = getRandomMove();
+    } while (lastMove && getFace(move) === getFace(lastMove));
+
+    scramble += move + ' ';
+    lastMove = move;
+  }
+
+  return scramble;
+}
+
+const Scramble3: React.FC = () => {
+  const [scramble, setScramble] = useState<string>('');
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.code === 'Space') {
+        setToggle((prevToggle) => !prevToggle);
+        if (!toggle) {
+          const newScramble = generateScramble(20);
+          setScramble(newScramble);
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [toggle]);
+
+  return (
+    <div>
+      <p>{scramble}</p>
+    </div>
+  );
+};
+
+export default Scramble3;
